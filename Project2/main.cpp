c1a658af0539d92b7f46f06e8cb71166f44f783d
yeonso08 <hjng0825@gmail.com>
2022-09-13 17:02:39 +0900

    c++ 기초

---
+++ Project2/main.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	//메모리 (0~4기가)내에 4칸 잡아줘
+	//정수 숫자 4칸에 저장하자( - 2^(32-1) ~ 2^(32-1) -1 ) 
+	//char : 1byte 저장(숫자로 저장하지만 출력은 문자(ascII))
+	//int : 4byte 저장(숫자,정수)
+	//float : 4byte 저장(숫자,실수)
+	//산술 연산자(+, -, *, /, %)
+	//int x = 'A' + 1;
+	//char x = 'A' + 1;
+
+	//int A = 9;
+	//int B = 6;
+
+
+	//std::cin >> A; // 입력 
+	//std::cin >> B;
+	//std::cout << A + B << std::endl;
+	//std::cout << (float)A / (float)B << std::endl; //형변환
+
+	//int korean = 100;
+	//int english = 90;
+	//int math = 80;
+	//int total = korean + english + math;
+	//float Average = (float)total / 3.0f;
+	//cout << Average << endl;
+
+	//int Korean[5] = { 10, 20, 30, 40, 50 };
+	//Korean[0] = 50;
+	//Korean[1] = 40;
+	//Korean[2] = 30;
+	//Korean[3] = 20;
+	//Korean[4] = 10;
+
+	//int Korean[10] = { 0, };
+	//int x = 0;
+
+	//for (x = 0; x < 10; x = x + 1)
+	//{
+	//	cin >> Korean[x];
+	//}
+
+	//if (10 < 9)
+	//{
+	//	cout << "true" << endl;
+	//}
+	//else
+	//{
+	//	cout << "false" << endl;
+	//}
+
+	//int Number = 0;
+
+	//cin >> Number;
+
+	//if (Number == 0)
+	//{
+	//	cout << "0" << endl;
+	//}
+	//else
+	//{
+	//	if (Number % 2 == 0)
+	//	{
+	//		cout << "짝수" << endl;
+	//	}
+	//	else
+	//	{
+	//		cout << "홀수" << endl;
+	//	}
+	//	}	
+
+	//int Score = 0;
+	//
+	//cin >> Score;
+
+	//if (Score >= 90)
+	//{
+	//cout << "A" << endl;
+	//}
+	//else if (Score >= 80)
+	//{
+	//	cout << "B" << endl;
+	//}
+	//else if (Score >= 70)
+	//{
+	//	cout << "C" << endl;
+	//}
+	//else if (Score >= 60)
+	//{
+	//	cout << "D" << endl;
+	//}
+	//else
+	//{
+	//	cout << "F" << endl;
+	//}
+	
+
+	//int sum = 0;
+
+	//for (int i = 0; i <= 100; i = i++)
+	//{
+	//	sum = sum + i; // sum += i
+
+	//}
+	//	cout << sum << endl;
+
+
+		//int sum = 0;
+
+		//for (int i = 3; i <= 100; i += 3)
+		//{
+		//	sum = sum + i;
+
+		//}
+
+		//for (int i = 0; i <= 100; i += 2)
+		//{
+		//	sum = sum + i;
+
+		//}
+		//
+		//for (int i = 1; i <= 100; i += 2)
+		//{
+		//	sum = sum + i;
+
+		//}
+		//
+		//cout << sum << endl;
+
+	int 
+
+
+	return 0;
+
+}
